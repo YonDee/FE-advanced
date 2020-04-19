413576bff645afa0f05c8f28cfde424d28185df4
YonDee <im.yondee@gmail.com>
2020-04-19 15:15:47 +0800

    快速实现一个todolist

--- demos/vue/todo-list/src/components/App.vue
+++ demos/vue/todo-list/src/components/App.vue
@@ -1,81 +1,61 @@
 <template>
-  <div id="app">
-    <div class="banner">
-      <img
-        src="https://vuejs.org/images/logo.png"
-        width="100"
-        alt="vue"
-        class="logo"
-      />
-      <h1>Welcome to Vue.js</h1>
-    </div>
-    <div class="bottom">
-      To get started, edit <code>./src/components/App.vue</code> and save to reload.<br/>
-      <span class="fade">
-        Checkout <code>./README.md</code> for more usages.
-      </span>
-    </div>
-  </div>
+<div>
+  <!-- 错误的写法，这样不会捕获到 value -->
+  <!-- <input @onchange="inputChange()" placeholder="What to plan to do?" /> -->
+  <!-- 正确的写法 -->
+  <input @input="inputChange()" v-model="inputContent" placeholder="What to plan to do?" />
+  <button @click="submit()">submit</button>
+  <ul>
+    <li v-for="(item, index) in contentsList" :key="`contnet-${index}`">
+      {{ item }}
+    </li>
+  </ul>
+</div>
 </template>
 
 <script>
-  export default {
-    name: 'app'
+export default {
+  data() {
+    return {
+      inputContent: '',
+      contentsList: []
+    }
+  },
+  beforeCreate() {
+    console.log('before create log')
+  },
+  created() {
+    console.log('created log')
+  },
+  beforeMount() {
+    console.log('before mount')
+  },
+  mounted() {
+    console.log('mounted')
+  },
+  beforeUpdate() {
+    console.log('before update log')
+  },
+  updated() {
+    console.log('updated log')
+  },
+  beforeDestroy() {
+    console.log('before destroy log')
+  },
+  destroyed() {
+    console.log(' destroyed log')
+  },
+  methods: {
+    inputChange() {
+      console.log('input change')
+    },
+    submit() {
+      console.log('added to do')
+      this.contentsList.push(this.inputContent)
+    }
   }
+}
 </script>
 
-<!-- CSS libraries -->
-<style src="normalize.css/normalize.css"></style>
-
-<!-- Global CSS -->
-<style>
-  code {
-    font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace, serif;
-    font-size: 0.9em;
-    white-space: pre-wrap;
-    color: #2c3e50;
-  }
-
-  code::before, code::after {
-    content: '`';
-  }
-</style>
-
-<!-- Scoped component css -->
-<!-- It only affect current component -->
 <style scoped>
-  #app {
-    text-align: center;
-  }
-
-  #app h1 {
-    color: #2c3e50;
-    font-weight: 300;
-    margin: 0;
-  }
-
-  .banner {
-    height: 200px;
-    background-color: #f6f6f6;
-    padding: 50px 10px;
-  }
-
-  .bottom {
-    padding: 80px 10px;
-    font-size: 24px;
-    font-weight: 300;
-  }
-
-  .fade {
-    font-size: 14px;
-  }
-
-  .logo {
-    animation: spin 4s 1s infinite linear
-  }
-
-  @keyframes spin {
-    from {transform:rotate(0deg);}
-    to {transform:rotate(360deg);}
-  }
 </style>
